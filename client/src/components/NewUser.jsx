0aad04412f7e7e150557fa08f6f6a1ecb8d1d763
Eric Penn <ep00959@georgiasouthern.edu>
2020-02-10 02:49:41 -0500

    forms

--- client/src/components/NewUser.jsx
+++ client/src/components/NewUser.jsx
@@ -55,16 +55,32 @@ export default class NewUser extends Component {
                     <button type='submit'>Submit</button>
                 </form>
                 <Form onSubmit={this.onCreateUserSubmit}>
-                    <input type='text' name="name" value={this.state.newUser.name} onChange={this.onChange} />>
-  <Form.Group controlId="formName">
-    <Form.Label>User Name</Form.Label>
-    <Form.Control type="text" placeholder="Enter your username" />
-  </Form.Group>
-
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
+                    <Form.Group controlId="formName">
+                        <Form.Label>User Name</Form.Label>
+                    <Form.Control type="text" name="name" value={this.state.newUser.name} onChange={this.onChange} placeholder="Enter your username" />
+                    </Form.Group>
+                    <Form.Group controlId="formAge">
+                        <Form.Label>Age</Form.Label>
+                    <Form.Control type="number" name="age" value={this.state.newUser.age} onChange={this.onChange} placeholder="Enter your age" />
+                    </Form.Group>
+                    <Form.Group controlId="formLocation">
+                        <Form.Label>Location</Form.Label>
+                    <Form.Control type="text" name="location" value={this.state.newUser.location} onChange={this.onChange} placeholder="Location?" />
+                    </Form.Group>
+                    <Form.Group controlId="formImage">
+                        <Form.Label>Link to Image</Form.Label>
+                    <Form.Control type="text" name="image" value={this.state.newUser.user_img} onChange={this.onChange} placeholder="Image URL?" />
+                    </Form.Group>
+                    <Form.Group controlId="exampleForm.ControlSelect2">
+                     <Form.Label>Example multiple select</Form.Label>
+                      <Form.Control as="select" multiple>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                        </Form.Control>
+                    </Form.Group>
   <Form.Group controlId="formBasicCheckbox">
     <Form.Check type="checkbox" label="Check me out" />
   </Form.Group>
